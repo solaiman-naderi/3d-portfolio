558d80e4f01e55037683532b9714f1b20cc95bb3
solaiman-naderi <solaimannaderi00@gmail.com>
2024-03-27 17:07:47 +0330

    added text

--- src/Experience.js
+++ src/Experience.js
@@ -1,4 +1,13 @@
-import { Float, Environment, useGLTF, OrbitControls } from "@react-three/drei";
+import {
+  PresentationControls,
+  Float,
+  Environment,
+  useGLTF,
+  OrbitControls,
+  ContactShadows,
+  Html,
+  Text,
+} from "@react-three/drei";
 
 export default function Experience() {
   const model = useGLTF("/model.gltf");
@@ -9,10 +18,48 @@ export default function Experience() {
 
       <color args={["#695b5b"]} attach={"background"} />
 
-      <OrbitControls makeDefault />
-      <Float>
-        <primitive object={model.scene} position-y={-1.2} />
-      </Float>
+      <PresentationControls
+        global
+        rotation={[0.13, 0.1, 0]}
+        polar={[-0.4, 0.2]}
+        azimuth={[-1, 0.75]}
+        config={{ mass: 2, tension: 400 }}
+        snap={{ mass: 4, tension: 400 }}
+      >
+        <Float>
+          <rectAreaLight
+            width={2.5}
+            height={1.65}
+            intensity={65}
+            color={"#2082a4"}
+            rotation={[-0.1, Math.PI, 0]}
+            position={[0, 0.55, -1.15]}
+          />
+          <primitive object={model.scene} position-y={-1.2}>
+            <Html
+              transform
+              wrapperClass="htmlIframe"
+              distanceFactor={1.17}
+              position={[0, 1.56, -1.4]}
+              rotation-x={-0.256}
+            >
+              <iframe src="https://panjerestudio.com/" />
+            </Html>
+          </primitive>
+          <Text
+            font="/bangers-v20-latin-regular.woff"
+            fontSize="1"
+            position={[2, 0.75, 0.75]}
+            rotation-y={-1.25}
+            maxWidth={2}
+            textAlign="center"
+          >
+            Panjere Studio
+          </Text>
+        </Float>
+      </PresentationControls>
+
+      <ContactShadows position-y={-1.4} opacity={0.8} scale={5} blur={2.4} />
     </>
   );
 }
